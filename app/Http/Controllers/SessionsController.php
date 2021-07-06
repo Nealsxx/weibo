9a4b3e46d8d98e8a5856a817952ea12399af1e52
Neal <asunxianxin@gmail.com>
2021-07-06 08:07:34 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,4 +30,14 @@ class SessionsController extends Controller
            return redirect()->back()->withInput();
        }
     }
+
+    /**
+     * 用户退出的相关逻辑
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 }
