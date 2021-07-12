3b8fc88b8053827c9086005aaad545adf8983f99
Neal <asunxianxin@gmail.com>
2021-07-12 05:22:34 +0000

    用户激活

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -26,6 +26,7 @@ class RedirectIfAuthenticated
                 session()->flash('info', '您已登录，无需再次操作。');
                 return redirect(RouteServiceProvider::HOME);
             }
+            return $next($request);
         }
     }
 }
